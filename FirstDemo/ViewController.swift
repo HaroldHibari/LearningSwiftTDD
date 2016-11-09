15f2c8f9c0c421cded1f3662c26b19e3fd88e91e
GC_Developer <gc_developer@GC-Developers-Mac-mini.local>
2016-11-09 16:08:31 +0000

    Initial Commit

---
+++ FirstDemo/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  FirstDemo
+//
+//  Created by GC_Developer on 09/11/2016.
+//  Copyright © 2016 GC_Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfVowelsShort(string: String) -> Int{
+        //create an array of Characters containing all the vowels
+        let vowels: [Character] = ["a","i","u","e","o","A","I","U","E","O"]
+        
+        return string.characters.reduce(0){$0 + (vowels.contains($1) ? 1 : 0)}
+    }        
+    
+    func numberOfVowelsLong(string: String) -> Int{
+        //create an array of Characters containing all the vowels
+        let vowels: [Character] = ["a","i","u","e","o","A","I","U","E","O"]
+        
+        //create a counter to store the nuber of vowels
+        var numberOfVowels = 0
+        
+        
+        //iterate through the characters in a given string and increment
+        //the counter if the current character is in the vowels array
+        for character in string.characters{
+            if vowels.contains(character){
+                numberOfVowels += 1
+            }
+        }
+        
+        return numberOfVowels
+    }
+
+    func makeHeadlineLong(string: String) -> String {
+        //split the string into words
+        let words = string.components(separatedBy: " ")
+        
+        var headline = ""
+        
+        //iterate through the words, taking out the first character, replacing
+        //it with an uppercase character and then concatenating it with the
+        //remaining characters in the word. Add this concatenation to the headline
+        //variable to remake the sentence
+        for var word in words{
+            let firstCharacter = word.remove(at: word.startIndex)
+            headline += "\(String(firstCharacter).uppercased())\(word) "
+        }
+        
+        //removes the last element in the headline which would be a space
+        headline.remove(at: headline.index(before: headline.endIndex))
+        
+        return headline
+    }
+    
+    func makeHeadlineShort(string: String) -> String{
+        
+        //returns an array which maps the words which have been split from the string.
+        //the mapping places the result of replacing the first character from the
+        //current word in each array slot and seperates these results by a space
+        return string.components(separatedBy: " ").map {
+                var word = $0
+                let firstCharacter = word.remove(at: word.startIndex)
+                return "\(String(firstCharacter).uppercased())\(word)"
+        }.joined(separator: " ")
+    }
+}
+
---
+++ FirstDemoTests/FirstDemoTests.swift
@@ -0,0 +1,87 @@
+//
+//  FirstDemoTests.swift
+//  FirstDemoTests
+//
+//  Created by GC_Developer on 09/11/2016.
+//  Copyright © 2016 GC_Developer. All rights reserved.
+//
+
+import XCTest
+@testable import FirstDemo
+
+class FirstDemoTests: XCTestCase {
+    
+    //create a variable thats of type ViewController
+    var viewController: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+    
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        //assign viewController variable to the ViewController
+        viewController = ViewController()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    /**
+     This test verifies if the vowel counter short version is working as intended
+     - String: the word that is used to test the function
+     */
+    func testNumberOfVowelsShort_ShouldReturnNumberOfVowels(){
+        let string = "ohayou" //defines the string to be used for testing
+        
+        //call the function we want to test, parsing through the string and assigns the result to a constant
+        let numberOfVowels = viewController.numberOfVowelsShort(string: string)
+        
+        //the assert verifies if the constant is equal to the actual number of vowels in the word
+        XCTAssertEqual(numberOfVowels,4,"should find 4 vowels in ohayou")
+        
+    }
+    
+    /**
+     This test verifies if the vowel counter long version is working as intended
+     - String: the word that is used to test the function
+     */
+    func testNumberOfVowelsLong_ShouldReturnNumberOfVowels(){
+        let string = "gozaimasu"
+       
+        let numberOfVowels = viewController.numberOfVowelsLong(string: string)
+        
+       XCTAssertEqual(numberOfVowels,5,"should find 5 vowels in gozaimasu")
+        
+        
+    }
+    
+    /**
+     This test verifies that the iterative function version to make every word start with a capital letter
+     when that word is passed through
+     */
+    func testMakeHeadlineLong_ShouldReturnEachWordStartsCapital(){
+        let inputString = "ohayou this is working fine"
+        let expectedHeadline = "Ohayou This Is Working Fine"
+        
+        let result = viewController.makeHeadlineLong(string: inputString)
+        
+        XCTAssertEqual(expectedHeadline, result)
+        
+    }
+    
+    /**
+     This test verifies that the remove function version to make every word start with a capital letter
+     when that word is passed through
+     */
+    func testMakeHeadlineShort_ShouldReturnEachWordStartsCapital(){
+        let inputString = "gozaimasu this is working fine"
+        let expectedHeadline = "Gozaimasu This Is Working Fine"
+        
+        let result = viewController.makeHeadlineShort(string: inputString)
+        
+        XCTAssertEqual(expectedHeadline, result)
+    }
+    
+}
